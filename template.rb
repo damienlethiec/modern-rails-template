4b8e503a8ee395a7a7ed2a6687a2a844cdc63ca1
Damien Le Thiec <damien.lethiec@gmail.com>
2018-02-07 14:19:15 +0700

    assert minimum rails version

---
+++ template.rb
@@ -0,0 +1,17 @@
+RAILS_REQUIREMENT = ">= 5.1.0"
+
+def apply_template!
+  assert_minimum_rails_version
+end
+
+def assert_minimum_rails_version
+  requirement = Gem::Requirement.new(RAILS_REQUIREMENT)
+  rails_version = Gem::Version.new(Rails::VERSION::STRING)
+  return if requirement.satisfied_by?(rails_version)
+
+  prompt = "This template requires Rails #{RAILS_REQUIREMENT}. "\
+           "You are using #{rails_version}. Continue anyway?"
+  exit 1 if no?(prompt)
+end
+
+apply_template!
